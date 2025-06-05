3ea708d6d9a5849f7d83bd8576926be99e8f4f52
Paul <123821602+Kap1tan@users.noreply.github.com>
2025-06-05 22:38:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Токен бота (получить у @BotFather)
-BOT_TOKEN = "8106146454:AAHa7tqqMgh_tW39FUPwdJ7yYj8GE6ViXj0"
+BOT_TOKEN = "8106146454:AAHPhmadRH_NVHbhBQQyOC_E7HZ1GVwK43K"
 
 
 # Состояния пользователя
